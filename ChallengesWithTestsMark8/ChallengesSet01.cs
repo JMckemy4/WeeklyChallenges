28e1822e29e9fe8fd66f29f9dc3ad641b5fc84a5
jared mckemy <jaredmkcemy4@gmail.com>
2023-09-17 17:37:30 -0700

    made changes

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,7 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
+            if (num1 == num2)
             {
                 return true;
             }
@@ -18,32 +18,50 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            double result = minuend - subtrahend;
+            return result;
         }
-
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int result = number1 + number2;
+            return result;
+
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long result = factor1 * factor2;
+            return result;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
-        }
-
+            if (nameOfPerson != "")
+            {
+              return $"Hello, {nameOfPerson}!"; 
+            }
+            else
+            {
+                return "Hello!";
+            }
+        }   
+        
         public string GetHey()
         {
-            throw new NotImplementedException();
+            string greeting = "HEY!";
+            return greeting;
         }
     }
 }
