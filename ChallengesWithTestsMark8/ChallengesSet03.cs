2ef7d1f3114d616167466049e70e8fbbb9560273
jared mckemy <jaredmkcemy4@gmail.com>
2023-09-22 07:54:48 -0700

    finished with all passing

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,124 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            if (vals == null)
+            {
+                throw new ArgumentNullException(nameof(vals));
+            }
+
+            foreach (bool value in vals)
+            {
+                if (!value)
+                {
+                    return true; // Found a 'false' value
+                }
+            }
+
+            return false; // No 'false' value found
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false; // Sum of nothing is considered even
+            }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 2 != 0) // Check if the number is odd
+                {
+                    sum += number;
+                }
+            }
+
+            return sum % 2 != 0; // Check if the sum is odd
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Password cannot be null or empty.", nameof(password));
+            }
+
+            return password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit);
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(val))
+            {
+                // Handle null or empty input string, depending on your requirements
+                throw new ArgumentException("Input string cannot be null or empty.", nameof(val));
+            }
+
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(val))
+            {
+                // Handle null or empty input string, depending on your requirements
+                throw new ArgumentException("Input string cannot be null or empty.", nameof(val));
+            }
+
+            return val[val.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                // Handle division by zero, returning 0 for this method
+                return 0;
+            }
+
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            if (nums == null || nums.Length == 0)
+            {
+                // Handle null or empty array, depending on your requirements
+                throw new ArgumentException("Input array cannot be null or empty.", nameof(nums));
+            }
+
+            int lastIndex = nums.Length - 1;
+            return nums[lastIndex] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            List<int> oddNumbers = new List<int>();
+
+            for (int i = 1; i < 100; i += 2)
+            {
+                oddNumbers.Add(i);
+            }
+
+            return oddNumbers.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null)
+            {
+                // Handle null input array, depending on your requirements
+                throw new ArgumentNullException(nameof(words));
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != null)
+                {
+                    words[i] = words[i].ToUpper();
+
+                }
+            }
         }
     }
 }
